b9ea5efd96e2e2ddc97a981236326e486e84fd1d
Benjamin HOLQUIN <holquinb@frene21.iut-infobio.priv.univ-lille1.fr>
2017-03-22 14:01:22 +0100

    creation de la table produits mg !

---
+++ server/src/main/java/fr/iutinfo/skeleton/api/ProduitsDao.java
@@ -0,0 +1,43 @@
+package fr.iutinfo.skeleton.api;
+
+import org.skife.jdbi.v2.sqlobject.*;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import org.skife.jdbi.v2.tweak.BeanMapperFactory;
+
+import java.util.List;
+
+public interface ProduitsDao {
+	
+	@SqlUpdate("create table produits(idp integer primry key autoincrement,"
+			+ "libelle text, reference text,prix float,description text, categorie text)")
+	void createProduitsTable();
+	
+    @SqlUpdate("insert into produits (libelle, reference, prix, description, categorie) values"
+    		+ " (:libelle, :reference, :prix, :description, :categorie)")
+    @GetGeneratedKeys
+    int insert(@BindBean() Produits produits);
+
+    @SqlQuery("select * from produits where libelle = :libelle")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    Produits findByLibelle(@Bind("libelle") String libelle);
+
+    @SqlQuery("select * from Produits where search like :libelle")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    List<Produits> search(@Bind("libelle") String libelle);
+
+    @SqlUpdate("drop table if exists produits")
+    void dropProduitsTable();
+
+    @SqlUpdate("delete from produits where idp = :idp")
+    void delete(@Bind("id") int id);
+
+    @SqlQuery("select * from produits order by idp")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    List<Produits> all();
+
+    @SqlQuery("select * from produits where idp = :idp")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    Produits findByIdp(@Bind("idp") int idp);
+
+    void close();
+}
