4c4865c42166bc1d2b98143cd6383e5d2c764693
Axel COEURET <axel.coeuret@etudiant.univ-lille1.fr>
2017-03-23 17:15:33 +0100

    animation + tri par categorie

--- server/src/main/webapp/js/script.js
+++ server/src/main/webapp/js/script.js
@@ -1,37 +1,7 @@
 $(document).ready(function(){
     
-    $('.nav li').click(function(){
-        $('.nav .active').removeClass('active');
-        $(this).addClass('active');
-        afficherContenu('#contenu');
-    });
-   $('.categorie').click(function(event){
-        var categorie = $(this).attr('id') ;
-        var txt = $(this).text();
-        $.ajax({
-            url : "v1/produits/categorie/" + categorie ,
-            type : "GET" ,
-            dataType : "json" ,
-            success : function(json){
-                $('#catalogue').empty() ;
-                console.log($(this).attr('id'));
-                $('#catalogue').append($('<h1>'+ txt +'</h1>'));
-                var articles = JSON.parse(JSON.stringify(json));
-                afficherArticles(articles);
-                 $('nav li .active').removeClass('active');
-        $(this).addClass('active');
-            } ,
-            error :  function( xhr, status, errorThrown  ){
-                alert( "Sorry, there was a problem!" );
-        console.log( "Error: " + errorThrown );
-        console.log( "Status: " + status );
-        console.dir( xhr );
-            }
-            
-            
-        })
-        
-    })
+   
+ 
         
     $('#btnConnection').click(function(){
         afficherContenu('#ContenuLogin');        
@@ -66,14 +36,64 @@ $(document).ready(function(){
         }
         catalogue.append(row);
     }  
+    var afficherArticlesDepart = function(articles){
+        var categorie = [];
+        var catalogue = $('#catalogue');
+        var row = $('<div class="row">');
+        for(var i=1; i<=articles.length; i++){
+            row.append(creerArticle(articles[i-1].id, articles[i-1].libelle, articles[i-1].description, articles[i-1].urlImage));
+            if(categorie.indexOf(articles[i-1].categorie) == -1){
+                categorie.push(articles[i-1].categorie);   
+            }
+            if(i%3 == 0){
+                catalogue.append(row);
+                var row = $('<div class="row">');
+            }
+        }
+        catalogue.append(row);
+        
+        for(var i=0; i<categorie.length; i++){
+            $('.nav-pills').append('<li role="presentation"  ><a href="#" class="categorie" id="'+categorie[i]+'">'+categorie[i]+'</a></li>');
+        }
+    }  
     var ajouterArticle = function(regex){
         $('#catalogue .row').append(regex);
     }
-
-    var article = creerArticle(1 , 'Moteur Basse performance' , "" , "http://www.moteur-diesel-marin-midif.fr/Files/111759/Img/10/moteur-diesel-inboard-midif-MD1760-52-cv-zoom.png" );
     
-    article.appendTo('#catalogue .row');
-    
+    var trierCategorie = function(){
+        $('.nav .active').removeClass('active');
+        $(this).addClass('active');
+        if( $('section.col-md-9:visible').attr('id') != "contenu" ){
+            afficherContenu('#contenu');
+        }else{
+            //$('#catalogue').hide();
+            $('#catalogue').addClass('fadeInDownBig');
+            setTimeout(function() { $('#catalogue').removeClass('fadeInDownBig'); } , 1000) ;
+        }
+        var categorie = $('.nav .active ').text() ;
+        
+        $.ajax({
+            url : "v1/produits/categorie/" + categorie ,
+            type : "GET" ,
+            dataType : "json" ,
+            success : function(json){
+                $('#catalogue').empty() ;
+                $('#catalogue').append($('<h1>'+ categorie +'</h1>'));
+                var articles = JSON.parse(JSON.stringify(json));
+                afficherArticles(articles);
+                 $('nav li .active').removeClass('active');
+        $(this).addClass('active');
+            } ,
+            error :  function( xhr, status, errorThrown  ){
+                alert( "Sorry, there was a problem!" );
+        console.log( "Error: " + errorThrown );
+        console.log( "Status: " + status );
+        console.dir( xhr );
+            }
+            
+            
+        })
+    };
     
     $.ajax({
       url: "v1/produits",
@@ -84,7 +104,8 @@ $(document).ready(function(){
         $('#catalogue').append($('<h1>Les indispensables</h1>'));
         var articles = JSON.parse(JSON.stringify(json));  
         console.log(articles);
-        afficherArticles(articles);
+        afficherArticlesDepart(articles);
+        $('.nav li').on('click' , trierCategorie) ;
       },
       error: function( xhr, status, errorThrown ) {
         alert( "Sorry, there was a problem!" );
@@ -92,7 +113,8 @@ $(document).ready(function(){
         console.log( "Status: " + status );
         console.dir( xhr );
       }
-    }); // end ajax
+    });
+    
 });
 
 
