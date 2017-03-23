c006e42d9aa13a666e8f27c8277e06c25f744a2c
Simon VANDEPUTTE <simon2310mike@gmail.com>
2017-03-23 10:25:18 +0100

    Modif

--- server/src/main/java/fr/iutinfo/skeleton/api/Produits.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/Produits.java
@@ -27,6 +27,14 @@ public class Produits {
 		this.urlImage = urlImage;
 	}
 	
+	public String getUrlImage(){
+		return urlImage;
+	}
+	
+	public void setUrlImage(String urlImage){
+		this.urlImage = urlImage;
+	}
+	
 	public String getLibelle() {
 		return libelle;
 	}
