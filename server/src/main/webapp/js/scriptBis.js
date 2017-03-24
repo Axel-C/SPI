a6225c2f69912457e85754dd15d50d4a67af1858
Adrien Deblock <deblock.adrien.62@gmail.com>
2017-03-24 09:40:19 +0100

    description produits

---
+++ server/src/main/webapp/js/scriptBis.js
@@ -0,0 +1,182 @@
+$(document).ready(function(){
+    $('#btnConnection').click(function(){
+        afficherContenu('#ContenuLogin');        
+    });
+    $('#nousContacter').click(function(){
+        afficherContenu('#contenuContact');
+    });
+    
+    var afficherContenu = function(div){
+        $('section.col-md-9:visible').hide(300);
+        $(div).show(300); 
+    }
+    var creerArticle = function( id , name , description  , img){
+         return $(" <article class='thumbnail col-md-4' >"+
+                  "<img src='"+ img + "' class='img-thumbnail img-responsive' alt='"+ name + "'>"+
+                  "<div class='caption' >"+
+                    "<h3>"+ name +"</h3>"+
+                 "<a href='#' class='btn btn-info enSavoirPlus' id='id"+id+"'>En savoir plus</a>"+
+                "</div>"+
+                   " </article>");
+    }   
+    var creerArticleDescription = function( id , name , description  , img){
+         return $(" <article class='thumbnail col-md-5' id='descriptionArticle' >"+
+                  "<img src='"+ img + "' class='img-thumbnail img-responsive' alt='"+ name + "'>"+
+                  "<div class='caption' >"+
+                    "<h3>"+ name +"</h3>"+
+                    "<p>"+description+"</p>"+
+                "</div>"+
+                   " </article>");
+    }  
+    var afficherArticles = function(articles){
+        
+        var catalogue = $('#catalogue');
+        var row = $('<div class="row">');
+        for(var i=1; i<=articles.length; i++){
+            row.append(creerArticle(articles[i-1].idp, articles[i-1].libelle, articles[i-1].description, articles[i-1].urlImage));
+            if(i%3 == 0){
+                catalogue.append(row);
+                var row = $('<div class="row">');
+            }
+        }
+        catalogue.append(row);
+    }  
+    var afficherArticle = function(articles){
+        var catalogue = $('#catalogue');
+        var row = $('<div class="row">');
+        row.append(creerArticleDescription(articles.idp, articles.libelle, articles.description, articles.urlImage));
+        catalogue.append(row);
+    }
+    var afficherArticlesDepart = function(articles){
+        var categorie = [];
+        var catalogue = $('#catalogue');
+        var row = $('<div class="row">');
+        for(var i=1; i<=articles.length; i++){
+            row.append(creerArticle(articles[i-1].idp, articles[i-1].libelle, articles[i-1].description, articles[i-1].urlImage));
+            if(categorie.indexOf(articles[i-1].categorie) == -1){
+                categorie.push(articles[i-1].categorie);   
+            }
+            if(i%3 == 0){
+                catalogue.append(row);
+                var row = $('<div class="row">');
+            }
+        }
+        catalogue.append(row);
+        
+        for(var i=0; i<categorie.length; i++){
+            $('.nav-pills').append('<li role="presentation"  ><a href="#" class="categorie" id="'+categorie[i]+'">'+categorie[i]+'</a></li>');
+        }
+    }  
+    var ajouterArticle = function(regex){
+        $('#catalogue .row').append(regex);
+    }
+    
+    var trierCategorie = function(){
+        $('.nav .active').removeClass('active');
+        $(this).addClass('active');
+        if( $('section.col-md-9:visible').attr('id') != "contenu" ){
+            afficherContenu('#contenu');
+        }else{
+            //$('#catalogue').hide();
+            $('#catalogue').addClass('fadeInDownBig');
+            setTimeout(function() { $('#catalogue').removeClass('fadeInDownBig'); } , 1000) ;
+        }
+        var categorie = $('.nav .active ').text() ;
+        
+        $.ajax({
+            url : "v1/produits/categorie/" + categorie ,
+            type : "GET" ,
+            dataType : "json" ,
+            success : function(json){
+                $('#catalogue').empty() ;
+                $('#catalogue').append($('<h1>'+ categorie +'</h1>'));
+                var articles = JSON.parse(JSON.stringify(json));
+                afficherArticles(articles);
+                 $('nav li .active').removeClass('active');
+                 $(this).addClass('active');
+                
+                $('.enSavoirPlus').click(function(){
+            var num = $(this).attr('id').charAt('2');
+            $.ajax({
+                url : "v1/produits/"+num,
+                type : "GET" ,
+                dataType : "json" ,
+                success : function(json){
+                    $('#catalogue').empty() ;
+                    var articles = JSON.parse(JSON.stringify(json));
+                    $('#catalogue').append($('<h1>'+ articles.libelle +'</h1>'));
+                    afficherArticle(articles);
+                    $('nav li .active').removeClass('active');
+                    $('nav li #'+articles.categorie).addClass('active');
+                } ,
+                error :  function( xhr, status, errorThrown  ){
+                    alert( "Sorry, there was a problem!" );
+                    console.log( "Error: " + errorThrown );
+                    console.log( "Status: " + status );
+                    console.dir( xhr );
+                }
+
+
+            })
+        });
+            } ,
+            error :  function( xhr, status, errorThrown  ){
+                alert( "Sorry, there was a problem!" );
+            console.log( "Error: " + errorThrown );
+            console.log( "Status: " + status );
+            console.dir( xhr );
+            }
+            
+            
+        })
+    };
+    
+    $.ajax({
+      url: "v1/produits",
+      type: "GET",
+      dataType : "json",
+      success: function( json ) {
+        $('#catalogue').empty();
+        $('#catalogue').append($('<h1>Les indispensables</h1>'));
+        var articles = JSON.parse(JSON.stringify(json));  
+        console.log(articles);
+        afficherArticlesDepart(articles);
+        $('.nav li').on('click' , trierCategorie) ;
+       
+          
+        $('.enSavoirPlus').click(function(){
+            var num = $(this).attr('id').charAt('2');
+            $.ajax({
+                url : "v1/produits/"+num,
+                type : "GET" ,
+                dataType : "json" ,
+                success : function(json){
+                    $('#catalogue').empty() ;
+                    var articles = JSON.parse(JSON.stringify(json));
+                    $('#catalogue').append($('<h1>'+ articles.libelle +'</h1>'));
+                    afficherArticle(articles);
+                    $('nav li .active').removeClass('active');
+                    $('nav li #'+articles.categorie).addClass('active');
+                } ,
+                error :  function( xhr, status, errorThrown  ){
+                    alert( "Sorry, there was a problem!" );
+                    console.log( "Error: " + errorThrown );
+                    console.log( "Status: " + status );
+                    console.dir( xhr );
+                }
+
+
+            })
+        });
+      },
+      error: function( xhr, status, errorThrown ) {
+        alert( "Sorry, there was a problem!" );
+        console.log( "Error: " + errorThrown );
+        console.log( "Status: " + status );
+        console.dir( xhr );
+      }
+    });
+    
+});
+
+
